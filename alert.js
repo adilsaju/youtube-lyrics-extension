b51f15a46754fcd02436646d8a74639b1868a322
adilsaju1 <adil.saju@flytxt.com>
2020-01-22 15:04:52 +0530

    some working, WiP on alert.js

---
+++ alert.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function(){
+    // alert('hello ' + document.location.href);
+{/* <script> */}
+    // window.onload=
+  track_share_url_sliced=`https://www.musixmatch.com/lyrics/Rachel-Platten/Fight-Song`
+  // track_url=`//www.musixmatch.com/lyrics/Rachel-Platten/Fight-Song`
+  url1=`${track_share_url_sliced}/embed?theme=dark`
+  url2=`${track_share_url_sliced}`
+  embed_html=`<div><iframe src="${url1}" style="border:none;background:transparent;" width="100%" height="380" border=0></iframe><a href="${url2}">View Fight Song Lyrics by Rachel Platten</a> at <a href="https://www.musixmatch.com/">Musixmatch</a></div>`
+  var new_element=document.createElement('div')
+  new_element.innerHTML=embed_html
+  document.querySelector('body').insertBefore(new_element,document.querySelector('body').firstChild)
+  // var eElement; // some E DOM instance
+  // var newFirstElement; //element which should be first in E
+  
+  // eElement.insertBefore(newFirstElement, eElement.firstChild);
+  //   </script>
+    // var div = document.getElementById("top_bar");
+    // alert(div.innerHTML);
+  });
--- embed_lyrics.js
+++ embed_lyrics.js
@@ -1,7 +1,18 @@
-// track_share_url_sliced=``
-track_share_url_sliced=`https://www.musixmatch.com/lyrics/Rachel-Platten/Fight-Song`
-// track_url=`//www.musixmatch.com/lyrics/Rachel-Platten/Fight-Song`
-url1=`${track_share_url_sliced}/embed?theme=dark`
-url2=`${track_share_url_sliced}`
-{/* <div><iframe src="${url1}" style="border:none;background:transparent;" width="100%" height="380" border=0></iframe><a href="${url2}">View Fight Song Lyrics by Rachel Platten</a> at <a href="https://www.musixmatch.com/">Musixmatch</a></div> */}
-{/* <div><iframe src="https://www.musixmatch.com/lyrics/Rachel-Platten/Fight-Song/embed?theme=dark" style="border:none;background:transparent;" width="100%" height="380" border=0></iframe><a href="https://www.musixmatch.com/lyrics/Rachel-Platten/Fight-Song">View Fight Song Lyrics by Rachel Platten</a> at <a href="https://www.musixmatch.com/">Musixmatch</a></div> */}
+// // track_share_url_sliced=``
+// track_share_url_sliced=`https://www.musixmatch.com/lyrics/Rachel-Platten/Fight-Song`
+// // track_url=`//www.musixmatch.com/lyrics/Rachel-Platten/Fight-Song`
+// url1=`${track_share_url_sliced}/embed?theme=dark`
+// url2=`${track_share_url_sliced}`
+// {/* <div><iframe src="${url1}" style="border:none;background:transparent;" width="100%" height="380" border=0></iframe><a href="${url2}">View Fight Song Lyrics by Rachel Platten</a> at <a href="https://www.musixmatch.com/">Musixmatch</a></div> */}
+// {/* <div><iframe src="https://www.musixmatch.com/lyrics/Rachel-Platten/Fight-Song/embed?theme=dark" style="border:none;background:transparent;" width="100%" height="380" border=0></iframe><a href="https://www.musixmatch.com/lyrics/Rachel-Platten/Fight-Song">View Fight Song Lyrics by Rachel Platten</a> at <a href="https://www.musixmatch.com/">Musixmatch</a></div> */}
+
+
+// var app = chrome.runtime.getBackgroundPage();
+
+function hello() {
+    chrome.tabs.executeScript({
+      file: 'alert.js'
+    }); 
+  }
+  
+  window.addEventListener('load', hello);
